42a3e973c43614cc32d17d2e10f5112fa2fab764
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-09-17 13:08:13 +0200

    sf

--- web/alcoholCalc/alcoholCalcApp/static/countdown.js
+++ web/alcoholCalc/alcoholCalcApp/static/countdown.js
@@ -1,22 +1,15 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+window.onload = function() {
+  var hour = 2;
+  var sec = 60;
+  setInterval(function() {
+    document.getElementById("timer").innerHTML = hour + " : " + sec;
+    sec--;
+    if (sec == 00) {
+      hour--;
+      sec = 60;
+      if (hour == 0) {
+        hour = 2;
+      }
+    }
+  }, 1000);
 }
-
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
