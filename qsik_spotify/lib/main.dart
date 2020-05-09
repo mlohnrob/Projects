31689b306486aba145a8a51abd027974484ef265
August Emmery-Funch <32144571+augu1352@users.noreply.github.com>
2020-05-09 16:21:42 +0200

    CREATE

---
+++ qsik_spotify/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QsikSpot());
+
+class QsikSpot extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
